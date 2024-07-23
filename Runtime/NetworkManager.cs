e4b5ad06e24345ae50c88af79a383c3d298613a9
cyhzz <1076357483@qq.com>
2024-07-23 23:48:31 +0800

    da

---
+++ Runtime/NetworkManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Newtonsoft.Json;
+using UnityEngine.Networking;
+using Com.A9.Singleton;
+
+namespace Com.A9.A9019
+{
+    public class NetworkManager : Singleton<NetworkManager>
+    {
+        public Queue<IEnumerator> corontine_queue = new Queue<IEnumerator>();
+        //Hierarchy
+        public bool is_internet;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            StartCoroutine(CoroutineCoordinator());
+        }
+        public void SendRequest(string addr, object t, bool full_type = false, Action<string> CallBack = null, Action OnFail = null)
+        {
+            corontine_queue.Enqueue(Post(addr, t, full_type, CallBack, OnFail));
+        }
+
+        public static IEnumerator Post(string url, object pst, bool full_type, Action<string> OnSuccessCallBack = null, Action OnFail = null)
+        {
+            if (instance.is_internet)
+            {
+                Debug.Log("net traffic!");
+                yield break;
+            }
+            instance.is_internet = true;
+
+            var str = "";
+            if (full_type)
+            {
+                JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
+                str = JsonConvert.SerializeObject(pst, settings);
+            }
+            else
+            {
+                str = JsonConvert.SerializeObject(pst);
+            }
+
+            byte[] bt = System.Text.Encoding.UTF8.GetBytes(str);
+
+            UnityWebRequest req = new UnityWebRequest(url, "POST", new DownloadHandlerBuffer(), new UploadHandlerRaw(bt));
+
+            req.SetRequestHeader("Content-Type", "application/json");
+            yield return req.SendWebRequest();
+            if (req.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(req.error);
+                OnFail?.Invoke();
+            }
+            else
+            {
+                Debug.Log(req.downloadHandler.text);
+                OnSuccessCallBack?.Invoke(req.downloadHandler.text);
+            }
+            instance.is_internet = false;
+        }
+
+        IEnumerator CoroutineCoordinator()
+        {
+            while (true)
+            {
+                while (corontine_queue.Count > 0)
+                    yield return StartCoroutine(corontine_queue.Dequeue());
+                yield return null;
+            }
+        }
+    }
+
+}
